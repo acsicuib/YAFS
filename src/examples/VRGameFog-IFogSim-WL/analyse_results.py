2858a046292bdabe57dffbb922454c6c90c371dc
DebRC <roychowdhurydebdoot@gmail.com>
2021-04-12 18:21:37 +0530

    Fixed for python3

--- src/examples/VRGameFog-IFogSim-WL/analyse_results.py
+++ src/examples/VRGameFog-IFogSim-WL/analyse_results.py
@@ -4,6 +4,7 @@ import pandas as pd
 # for size in [100,1000,10000,100000,1000000]:
 time_loops = [["M.EGG", "M.Sensor", "M.Concentration"]]
 import matplotlib.pyplot as plt
+from yafs.stats import Stats
 
 #### ANALYSE FILES YAFS
 
@@ -17,19 +18,18 @@ for idx1, depth in enumerate([4, 8, 12, 16]):
      # 1000, 10000,
      for idx2,size in enumerate([1000,10000,100000]):
      #     size = 100000
-         print "DEPTH: %i | TIME: %s" %(depth , size)
+         print("DEPTH: %i | TIME: %s" %(depth , size))
          s = Stats(defaultPath="files/Results_%s_%s_%s"%(police,size,depth))
 
          #Network
          s.showResults2(size, time_loops=time_loops)
 
-         print "\t Bytes transmitted: " ,s.bytes_transmitted()
-         print "\t Messages transmitted: ",s.count_messages()
-
-         print "\t- Network saturation -"
-         print "\t\tAverage waiting messages : %i" % s.average_messages_not_transmitted()
-         print "\t\tPeak of waiting messages : %i" % s.peak_messages_not_transmitted()
-         print "\t\tTOTAL messages not transmitted: %i" % s.messages_not_transmitted()
+         print("\t Bytes transmitted: " ,s.bytes_transmitted())
+         print("\t Messages transmitted: ",s.count_messages())
+         print("\t- Network saturation -")
+         print("\t\tAverage waiting messages : %i" % s.average_messages_not_transmitted())
+         print("\t\tPeak of waiting messages : %i" % s.peak_messages_not_transmitted())
+         print("\t\tTOTAL messages not transmitted: %i" % s.messages_not_transmitted())
 
          #LOOPS
          # res = s.showLoops(time_loops)
@@ -39,7 +39,7 @@ for idx1, depth in enumerate([4, 8, 12, 16]):
          # print s.times("time_total_response")
          #
          # print "Latency Acc: ", s.df_link["latency"].sum()
-         print "*"*40
+         print("*"*40)
 
 
 police = "edge"
