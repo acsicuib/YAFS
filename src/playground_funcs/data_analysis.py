1977b011257edc3c78d364c5fe2d1766e5fa2783
sjooaaoo <santoshpjoao2003@gmail.com>
2023-05-26 19:53:49 +0100

    lixo removido

--- src/playground_funcs/data_analysis.py
+++ src/playground_funcs/data_analysis.py
@@ -44,16 +44,9 @@ def plot_paths_taken(folder_results):
 
     pallet = np.linspace(0, 100, len(apps_deployed))
 
-    colors = np.array([])
-    srcs = np.array([])
-    dsts = np.array([])
-
     fig, ax = plt.subplots()
 
     for en, a in enumerate(apps_deployed):
-        # colors = np.append(colors, (pallet[en]*np.ones(len(dfl[dfl.app == a].src))))
-        # srcs = np.append(srcs, np.array(dfl[dfl.app == a].src))
-        # dsts = np.append(dsts, np.array(dfl[dfl.app == a].dst))
 
         ax.scatter(np.array(dfl[dfl.app == a].src), np.array(dfl[dfl.app == a].dst), c=(pallet[en]*np.ones(len(dfl[dfl.app == a].src))), cmap='magma', vmin=0, vmax=100, marker='x', label=f'App: {a}')
 
