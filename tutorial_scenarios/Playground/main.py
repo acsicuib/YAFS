86eabe947099b0fb553c51ea845c10c01ee35fcd
sjooaaoo <santoshpjoao2003@gmail.com>
2023-07-10 23:52:05 +0100

    alteracoes em placement

--- tutorial_scenarios/Playground/main.py
+++ tutorial_scenarios/Playground/main.py
@@ -73,7 +73,9 @@ def placement_algorithm(graph, app_def='data/appDefinition.json'):
     apps = json.load(open(app_def))
 
     n_comunitties = len(apps)
-    comms = nx.algorithms.community.asyn_fluidc(graph, n_comunitties)#.__next__()
+    comms = nx.algorithms.community.asyn_fluidc(graph, n_comunitties)
+    spare_nodes = []
+    alloc_missing = []
 
     for app in apps:
         comms = comms.__next__()
@@ -81,12 +83,23 @@ def placement_algorithm(graph, app_def='data/appDefinition.json'):
             temp_dict = dict()
             temp_dict['module_name'] = mod['name']
             temp_dict['app'] = app['id']
-            temp_dict['id_resource'] = min(comms)
-            comms.discard(min(comms))
 
-            alloc['initialAllocation'].append(temp_dict)
+            if len(comms) != 0:
+                # Se ainda existir algum node da comunidade, utiliza-o
+                temp_dict['id_resource'] = min(comms)
+                comms.discard(min(comms))
+                alloc['initialAllocation'].append(temp_dict)
 
-            print(temp_dict) #
+            else:
+                # Senao, utilizará um que estiver de sobra
+                alloc_missing.append(temp_dict)
+
+        spare_nodes += list(comms)
+
+    for remaining in alloc_missing:
+        # Atribui um dos nós sem recursos
+        remaining['id_resource'] = spare_nodes.pop(0)
+        alloc['initialAllocation'].append(remaining)
 
     with open('data/allocDefinition.json', 'w') as f:
         json.dump(alloc, f)
