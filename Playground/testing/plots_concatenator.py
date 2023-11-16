b7a89e9cd384f48d79f4289a584de87f516413e6
Miguel-MotaCruz <96994533+Miguel-MotaCruz@users.noreply.github.com>
2023-11-16 11:26:32 +0000

    plots concatenator accepts name of file as argument

--- Playground/testing/plots_concatenator.py
+++ Playground/testing/plots_concatenator.py
@@ -1,33 +1,68 @@
-from PIL import Image
+# from PIL import Image
+# import os
+#
+# current_directory = os.path.dirname(__file__)
+#
+# # Directory containing the PNG files
+#
+# directory =  os.path.join(current_directory, 'data_analysis')
+#
+# # Get a list of PNG files in the directory
+# png_files = sorted([file for file in os.listdir(directory) if file.endswith('.png')])
+#
+# # Open each PNG file and append them vertically
+# images = [Image.open(os.path.join(directory, file)) for file in png_files]
+# widths, heights = zip(*(i.size for i in images))
+#
+# # Calculate the total width and height for the final image
+# total_width = max(widths)
+# total_height = sum(heights)
+#
+# # Create a new blank image with the calculated dimensions
+# concatenated_image = Image.new('RGB', (total_width, total_height), color = 'white')
+#
+# # Paste each image into the new image vertically
+# y_offset = 0
+# for img in images:
+#     x_offset = (total_width - img.width) // 2
+#
+#     concatenated_image.paste(img, (x_offset, y_offset))
+#     y_offset += img.size[1]
+#
+# # Save the final concatenated image
+# concatenated_image.save('./data_analysis/concatenated_image.png')
+
 import os
+import argparse
+from PIL import Image
 
-current_directory = os.path.dirname(__file__)
+# Function to concatenate images vertically
+def concatenate_images(output_filename):
+    current_directory = os.path.dirname(__file__)
+    directory = os.path.join(current_directory, 'data_analysis')
+    png_files = sorted([file for file in os.listdir(directory) if file.endswith('.png')])
 
-# Directory containing the PNG files
+    images = [Image.open(os.path.join(directory, file)) for file in png_files]
+    widths, heights = zip(*(i.size for i in images))
 
-directory =  os.path.join(current_directory, 'data_analysis')
+    total_width = max(widths)
+    total_height = sum(heights)
 
-# Get a list of PNG files in the directory
-png_files = sorted([file for file in os.listdir(directory) if file.endswith('.png')])
+    concatenated_image = Image.new('RGB', (total_width, total_height), color='white')
 
-# Open each PNG file and append them vertically
-images = [Image.open(os.path.join(directory, file)) for file in png_files]
-widths, heights = zip(*(i.size for i in images))
+    y_offset = 0
+    for img in images:
+        x_offset = (total_width - img.width) // 2
+        concatenated_image.paste(img, (x_offset, y_offset))
+        y_offset += img.size[1]
 
-# Calculate the total width and height for the final image
-total_width = max(widths)
-total_height = sum(heights)
+    output_path = os.path.join(directory, output_filename)
+    concatenated_image.save(output_path)
+    print(f"Concatenated plots saved as '{output_filename}' in '{directory}' directory.")
 
-# Create a new blank image with the calculated dimensions
-concatenated_image = Image.new('RGB', (total_width, total_height), color = 'white')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Concatenate PNG images vertically.')
+    parser.add_argument('output_filename', help='Name of the concatenated image file')
 
-# Paste each image into the new image vertically
-y_offset = 0
-for img in images:
-    x_offset = (total_width - img.width) // 2
-
-    concatenated_image.paste(img, (x_offset, y_offset))
-    y_offset += img.size[1]
-
-# Save the final concatenated image
-concatenated_image.save('./data_analysis/concatenated_image.png')
+    args = parser.parse_args()
+    concatenate_images(args.output_filename)
